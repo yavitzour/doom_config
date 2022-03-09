2f85d947af0fded5fbd541386de2f85bbc9d4bac
Yoav Avitzour <yavitzour@gmail.com>
2022-03-09 17:40:41 +0200

    Using multiple profiles using Chemacs2

---
+++ .emacs-profiles.el
@@ -0,0 +1,12 @@
+;;; package --- Summary
+
+;;; Commentary:
+;; Emacs profiles configuration
+
+;;; Code:
+(("doom-full" . ((user-emacs-directory . "~/.emacs.d.doom-full")
+                 (env . (("DOOMDIR" . "~/.doom.d.full")))))
+ ("doom-thin" . ((user-emacs-directory . "~/.emacs.d.doom-thin")
+                 (env . (("DOOMDIR" . "~/.doom.d.thin"))))))
+
+;;; .emacs-profiles.el ends here
